4f88484a4cd1e09b1cb730d6128a895125546523
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2017-10-15 08:04:48 +0700

    create project

---
+++ src/main/java/com/idspring/handlerinterceptordemo/HandlerInterceptorDemoApplication.java
@@ -0,0 +1,12 @@
+package com.idspring.handlerinterceptordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlerInterceptorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlerInterceptorDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/idspring/handlerinterceptordemo/HandlerInterceptorDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.idspring.handlerinterceptordemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HandlerInterceptorDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
