8995faf7ae94f2bc11f42ad39381e4d77825b378
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2017-10-21 17:38:47 +0700

    upgrade to spring 5

--- src/main/java/com/idspring/handlerinterceptordemo/DemoConfiguration.java
+++ src/main/java/com/idspring/handlerinterceptordemo/DemoConfiguration.java
@@ -2,14 +2,14 @@ package com.idspring.handlerinterceptordemo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Eko Kurniawan Khannedy
  * @since 15/10/17
  */
 @Configuration
-public class DemoConfiguration extends WebMvcConfigurerAdapter {
+public class DemoConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
